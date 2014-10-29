533b26a0a40fc6d12d37d186ff282a37327b5c95
kgorin <machineshopflow@gmail.com>
2014-10-29 18:39:30 +0300

    access -> access_token

--- lib/omniauth/strategies/coub.rb
+++ lib/omniauth/strategies/coub.rb
@@ -19,7 +19,7 @@ module OmniAuth
       end
 
       def raw_info
-        @raw_info ||= access.get("api/v2/users/me").parsed
+        @raw_info ||= access_token.get("api/v2/users/me").parsed
       end
 
     end
