9812a551ea6fe70bbfe085309cc3ef2f5db17f28
Mikhail Tabunov <tmp.over@gmail.com>
2014-10-29 17:50:33 +0300

    Now you can set custom coub host

--- lib/omniauth/strategies/coub.rb
+++ lib/omniauth/strategies/coub.rb
@@ -3,13 +3,13 @@ require 'omniauth-oauth2'
 module OmniAuth
   module Strategies
     class Coub < OmniAuth::Strategies::OAuth2
+      COUB_HOST = ENV['OA_COUB_HOST'] || "coub.com"
 
       option :name, 'coub'
-
       option :client_options, {
-        :site => 'http://coub.com/api/v2',
-        :authorize_url => 'http://coub.com/oauth2/authorize',
-        :token_url => 'http://coub.com/oauth2/token.json'
+        :site => "http://#{COUB_HOST}/api/v2",
+        :authorize_url => "http://#{COUB_HOST}/oauth2/authorize",
+        :token_url => "http://#{COUB_HOST}/oauth2/token.json"
       }
 
       uid { raw_info['id'] }
