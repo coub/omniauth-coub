aa335bd2f1799bc90cfa36aa4c7a683c1e3604ad
Mikhail Tabunov <tmp.over@gmail.com>
2014-11-07 21:23:32 +0300

    bump

--- lib/omniauth/strategies/coub.rb
+++ lib/omniauth/strategies/coub.rb
@@ -19,7 +19,7 @@ module OmniAuth
       end
 
       def raw_info
-        @raw_info ||= access.get("api/v2/users/me").parsed
+        @raw_info ||= access.get("sers/me").parsed
       end
 
     end
